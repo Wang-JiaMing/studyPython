4d9b1178f18527546e2918e525d88d250567c13e
wangjiaming <519677263@qq.com>
2017-07-19 16:14:58 +0800

    study

--- test/helloworld.py
+++ test/helloworld.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 
-str="hello world1111"
+num=1
+print(num)
 
-print (str)
+str = "Hello World!"
+print (str)        # 输出完整字符串
+print (str[0] )       # 输出字符串中的第一个字符
+print (str[2:5] )    # 输出字符串中第三个至第五个之间的字符串
+print (str[2:])       # 输出从第三个字符开始的字符串
+print (str * 2  )     # 输出字符串两次
+print (str + "TEST")  # 输出连接的字符串
